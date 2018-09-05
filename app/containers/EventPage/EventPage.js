5021e0deb91e76382009384a254b93aafb70834a
Roman Unt <untroman@mail.ru>
2018-09-05 13:27:03 +0300

    New favicon + event page info

--- app/containers/EventPage/EventPage.js
+++ app/containers/EventPage/EventPage.js
@@ -7,6 +7,7 @@ import { Column, Row, Container } from 'react-rasta';
 import { Switch, Route } from 'react-router-dom';
 import styled from 'styled-components';
 import propTypes from 'prop-types';
+import { COLOR_WHITE } from 'styles/styles-variables';
 
 import Page from 'components/Page';
 
@@ -27,13 +28,20 @@ const EventPageBackground = styled.div`
   min-height: 50%;
   width: 100%;
   filter: blur(8px);
-  mask-image: linear-gradient(to bottom, rgba(0,0,0,.3), rgba(0,0,0,0));
+  mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0));
   z-index: 0;
 `;
 
 const EventPageInfo = styled(Container)`
   position: relative;
   z-index: 1;
+  color: ${COLOR_WHITE};
+`;
+
+const EventPageInfoImage = styled.div`
+  background: url(${(props) => props.background}) 50% 50%;
+  background-size: cover;
+  height: 450px;
 `;
 
 export default class TicketPageComponent extends React.Component {
@@ -41,21 +49,39 @@ export default class TicketPageComponent extends React.Component {
     this.props.getEvent(this.props.match.params.id);
   }
   render() {
-    return (
-      <EventPage >
-        <Helmet>
-          <title>Tickets</title>
-          <meta
-            name="description"
-            content="Feature page of React.js Boilerplate application"
-          />
-        </Helmet>
-        <EventPageBackground background={this.props.event.picture} />
-        < EventPageInfo>
-          <div>123</div>
-        </EventPageInfo>
-      </EventPage>
-    );
+    if (this.props.event) {
+      return (
+        <EventPage>
+          <Helmet>
+            <title>{this.props.event.title}</title>
+          </Helmet>
+          <EventPageBackground background={this.props.event.picture} />
+          <EventPageInfo>
+            <Row>
+              <Column
+                size={{
+                  sm: 12,
+                  md: 4
+                }}
+              >
+                <EventPageInfoImage background={this.props.event.picture} />
+              </Column>
+              <Column
+                size={{
+                  sm: 12,
+                  md: 8
+                }}
+              >
+                <h1>{this.props.event.title}</h1>
+                <p>{this.props.event.place}</p>
+                <p>{this.props.event.date.full}</p>
+                <p>{this.props.event.about}</p>
+              </Column>
+            </Row>
+          </EventPageInfo>
+        </EventPage>);
+    }
+    return null;
   }
 }
 
